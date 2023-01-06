4d5c1cda381ab731c5c6e794bea45f9ab335e417
owlCode <akoladeo890@yahoo.com>
2023-01-06 20:24:24 +0100

    worked on the cart file

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,169 +2,83 @@ import { FaTimes } from "react-icons/fa";
 function Cart() {
    return (
       <div className="cart">
-         <div className="cart__box">
-            <h1 className="cart__heading">Shopping Cart</h1>
-            <div className="cart__body">
-               <ul className="cart__items">
-                  <div className="cart__item">
-                     <img className="cart__img" src="/mi-25.webp" alt="" />
-                     <div className="cart__details">
-                        <div className="cart__details-box-1">
-                           <div className="cart__name-box">
-                              <span>Midnight Gown-ish</span>
-                              <span>
-                                 <FaTimes color="#000" />
-                              </span>
-                           </div>
-                           <div className="cart__size-color">
-                              <span>Black/Medium</span>
-                           </div>
-                           <div className="cart__price-box">
-                              <span>$2000</span>
-                           </div>
+         <h1 className="cart__heading">Shopping Cart</h1>
+         <div className="cart__body">
+            <div className="cart__items">
+               <div className="cart__item">
+                  <img className="cart__img" src="/mi-25.webp" alt="" />
+                  <div className="cart__details">
+                     <div className="cart__details-box-1">
+                        <div className="cart__name-box">
+                           <span className="cart__product-name">Midnight Gown-ish</span>
+                           <span className="cart__cancel">x</span>
                         </div>
-                        <div className="cart__details-box-2">
-                           <div>
-                              <button>-</button>
-                              <span>4</span>
-                              <button>+</button>
-                           </div>
+                        <div className="cart__size-color">
+                           <span>Black / Medium</span>
+                        </div>
+                        <div className="cart__price-box">
+                           <span>$2000</span>
+                        </div>
+                     </div>
+                     <div className="cart__details-box-2">
+                        <div className="cart__quality">
+                           <button>-</button>
+                           <span>4</span>
+                           <button>+</button>
                         </div>
                      </div>
                   </div>
-                  <div className="cart__item">
-                     <img className="cart__img" src="/mi-25.webp" alt="" />
-                     <div className="cart__details">
-                        <div className="cart__details-box-1">
-                           <div className="cart__name-box">
-                              <span>Midnight Gown-ish</span>
-                              <span>
-                                 <FaTimes color="#000" />
-                              </span>
-                           </div>
-                           <div className="cart__size-color">
-                              <span>Black/Medium</span>
-                           </div>
-                           <div className="cart__price-box">
-                              <span>$2000</span>
-                           </div>
+               </div>
+               <div className="cart__item">
+                  <img className="cart__img" src="/mi-25.webp" alt="" />
+                  <div className="cart__details">
+                     <div className="cart__details-box-1">
+                        <div className="cart__name-box">
+                           <span className="cart__product-name">Midnight Gown-ish</span>
+                           <span className="cart__cancel">x</span>
                         </div>
-                        <div className="cart__details-box-2">
-                           <div>
-                              <button>-</button>
-                              <span>4</span>
-                              <button>+</button>
-                           </div>
+                        <div className="cart__size-color">
+                           <span>Black / Medium</span>
+                        </div>
+                        <div className="cart__price-box">
+                           <span>$2000</span>
+                        </div>
+                     </div>
+                     <div className="cart__details-box-2">
+                        <div className="cart__quality">
+                           <button>-</button>
+                           <span>4</span>
+                           <button>+</button>
                         </div>
                      </div>
                   </div>
-                  <div className="cart__item">
-                     <img className="cart__img" src="/mi-25.webp" alt="" />
-                     <div className="cart__details">
-                        <div className="cart__details-box-1">
-                           <div className="cart__name-box">
-                              <span>Midnight Gown-ish</span>
-                              <span>
-                                 <FaTimes color="#000" />
-                              </span>
-                           </div>
-                           <div className="cart__size-color">
-                              <span>Black/Medium</span>
-                           </div>
-                           <div className="cart__price-box">
-                              <span>$2000</span>
-                           </div>
+               </div>
+               <div className="cart__item">
+                  <img className="cart__img" src="/mi-25.webp" alt="" />
+                  <div className="cart__details">
+                     <div className="cart__details-box-1">
+                        <div className="cart__name-box">
+                           <span className="cart__product-name">Midnight Gown-ish</span>
+                           <span className="cart__cancel">x</span>
                         </div>
-                        <div className="cart__details-box-2">
-                           <div>
-                              <button>-</button>
-                              <span>4</span>
-                              <button>+</button>
-                           </div>
+                        <div className="cart__size-color">
+                           <span>Black / Medium</span>
+                        </div>
+                        <div className="cart__price-box">
+                           <span>$2000</span>
+                        </div>
+                     </div>
+                     <div className="cart__details-box-2">
+                        <div className="cart__quality">
+                           <button>-</button>
+                           <span>4</span>
+                           <button>+</button>
                         </div>
                      </div>
                   </div>
-                  <div className="cart__item">
-                     <img className="cart__img" src="/mi-25.webp" alt="" />
-                     <div className="cart__details">
-                        <div className="cart__details-box-1">
-                           <div className="cart__name-box">
-                              <span>Midnight Gown-ish</span>
-                              <span>
-                                 <FaTimes color="#000" />
-                              </span>
-                           </div>
-                           <div className="cart__size-color">
-                              <span>Black/Medium</span>
-                           </div>
-                           <div className="cart__price-box">
-                              <span>$2000</span>
-                           </div>
-                        </div>
-                        <div className="cart__details-box-2">
-                           <div>
-                              <button>-</button>
-                              <span>4</span>
-                              <button>+</button>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="cart__item">
-                     <img className="cart__img" src="/mi-25.webp" alt="" />
-                     <div className="cart__details">
-                        <div className="cart__details-box-1">
-                           <div className="cart__name-box">
-                              <span>Midnight Gown-ish</span>
-                              <span>
-                                 <FaTimes color="#000" />
-                              </span>
-                           </div>
-                           <div className="cart__size-color">
-                              <span>Black/Medium</span>
-                           </div>
-                           <div className="cart__price-box">
-                              <span>$2000</span>
-                           </div>
-                        </div>
-                        <div className="cart__details-box-2">
-                           <div>
-                              <button>-</button>
-                              <span>4</span>
-                              <button>+</button>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="cart__item">
-                     <img className="cart__img" src="/mi-25.webp" alt="" />
-                     <div className="cart__details">
-                        <div className="cart__details-box-1">
-                           <div className="cart__name-box">
-                              <span>Midnight Gown-ish</span>
-                              <span>
-                                 <FaTimes color="#000" />
-                              </span>
-                           </div>
-                           <div className="cart__size-color">
-                              <span>Black/Medium</span>
-                           </div>
-                           <div className="cart__price-box">
-                              <span>$2000</span>
-                           </div>
-                        </div>
-                        <div className="cart__details-box-2">
-                           <div>
-                              <button>-</button>
-                              <span>4</span>
-                              <button>+</button>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </ul>
-               <div className="cart__order-summary"></div>
+               </div>
             </div>
+            <div className="cart__order-summary"></div>
          </div>
       </div>
    );
