131db4ee23763b3aafb8e44b6667ad7bf391b462
owlCode <akoladeo890@yahoo.com>
2023-01-08 20:33:11 +0100

    worked on the cart file again

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -150,7 +150,72 @@ function Cart() {
                   <li className="cart__list cart__list--3">
                      <span>Shipping</span>
                      <span>Enter your address for easy and quick shipping.</span>
-                     <div className="cart__shipping-options"></div>
+                     <div className="cart__shipping-options">
+                        <div className="cart__select-box">
+                           <select className="cart__select" name="country" id="country">
+                              <option value="">Select a country</option>
+                              <option value="nigeria">Nigeria</option>
+                              <option value="ghana">Ghana</option>
+                              <option value="egypt">Egypt</option>
+                           </select>
+                        </div>
+
+                        <div className="cart__select-box">
+                           <select class="cart__select" name="states" id="states">
+                              <option value="">SELECT A STATE</option>
+                              <option value="abia">Abia</option>
+                              <option value="adamawa">Adamawa</option>
+                              <option value="akwa ibom">Akwa Ibom</option>
+                              <option value="anambra">Anambra</option>
+                              <option value="bauchi">Bauchi</option>
+                              <option value="bayelsa">Bayelsa</option>
+                              <option value="benue">Benue</option>
+                              <option value="borno">Borno</option>
+                              <option value="cross river">Cross River</option>
+                              <option value="delta">Delta</option>
+                              <option value="ebonyi">Ebonyi</option>
+                              <option value="edo">Edo</option>
+                              <option value="ekiti">Ekiti</option>
+                              <option value="enugu">Enugu</option>
+                              <option value="gombe">Gombe</option>
+                              <option value="imo">Imo</option>
+                              <option value="jigawa">Jigawa</option>
+                              <option value="kaduna">Kaduna</option>
+                              <option value="kano">Kano</option>
+                              <option value="katsina">Katsina</option>
+                              <option value="kebbi">Kebbi</option>
+                              <option value="kogi">Kogi</option>
+                              <option value="kwara">Kwara</option>
+                              <option value="lagos">Lagos</option>
+                              <option value="nasarawa">Nasarawa</option>
+                              <option value="niger">Niger</option>
+                              <option value="ogun">Ogun</option>
+                              <option value="ondo">Ondo</option>
+                              <option value="osun">Osun</option>
+                              <option value="oyo">Oyo</option>
+                              <option value="plateau">Plateau</option>
+                              <option value="rivers">Rivers</option>
+                              <option value="sokoto">Sokoto</option>
+                              <option value="taraba">Taraba</option>
+                              <option value="yobe">Yobe</option>
+                              <option value="zamfara">Zamfara</option>
+                              <option value="federal capital">Federal Capital</option>
+                           </select>
+                        </div>
+
+                        <div className="cart__select-box">
+                           <select className="cart__select" name="country" id="country">
+                              <option value="">Town / City</option>
+                              <option value="nigeria">Ondo city</option>
+                              <option value="ghana">Akure city</option>
+                              <option value="egypt">Lagos city</option>
+                           </select>
+                        </div>
+
+                        <button className="cart__coupon-btn cart__coupon-btn--update">
+                           UPDATE
+                        </button>
+                     </div>
                   </li>
                </ul>
             </div>
